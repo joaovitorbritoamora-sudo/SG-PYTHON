2cefa7a6df4ede947db19818befbb245b05ddaf2
joaovitorbritoamora-sudo <joao.vitor.brito.amora@gmail.com>
2026-05-08 19:51:39 -0300

    Create atividade4elif.py

---
+++ atividade4elif.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade "))
+experiencia = int(input("Digite True se tiver experiencia e False caso contrario "))
+AntecedenciasCriminais = int(input("Digite True se voce tem antecedencias criminais e False se nao "))
+experiencia = int(input("digite True caso tenha experiencia e False se nao" ))
+EnsinoSuperiorCompleto = int(input("Digite True se tiver ensino superior completo e False se nao  "))
+indicacao = int(input("Se tiver incacao digite True caso contrario False " ))
+
+if idade <= 18 :
+    print ("rejeitado")
