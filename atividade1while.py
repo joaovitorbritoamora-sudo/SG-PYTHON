3ec4bb7244f2db9e8f21a38b895f7efe93551801
joaovitorbritoamora-sudo <joao.vitor.brito.amora@gmail.com>
2026-05-22 20:34:47 -0300

    Update atividade1while.py

--- atividade1while.py
+++ atividade1while.py
@@ -1,9 +1,10 @@
-NumeroEscolhido = int(input("Digite um numero de 1 a 10"))
-numeroCorreto = 8
+NumeroCorreto = 8
+NumeroEscolhido = 75648
 while NumeroCorreto != NumeroEscolhido :
+    NumeroEscolhido = int(input("Digite um numero de 1 a 10: "))
     if NumeroCorreto > NumeroEscolhido :
         print ("O numero eh maior")
-    elif numeroCorreto == NumeroEscolhido:
+    elif NumeroCorreto == NumeroEscolhido:
         print ("Boa meu")
     else :
         print ("O numero eh menor")
