90bf7273c85759ffe71f604fabd607f2fd952534
joaovitorbritoamora-sudo <joao.vitor.brito.amora@gmail.com>
2026-05-22 19:48:02 -0300

    Create atividade1while.py

---
+++ atividade1while.py
@@ -0,0 +1,10 @@
+NumeroEscolhido = int(input("Digite um numero de 1 a 10"))
+numeroCorreto = 8
+while NumeroCorreto != NumeroEscolhido :
+    if NumeroCorreto > NumeroEscolhido :
+        print ("O numero eh maior")
+    elif numeroCorreto == NumeroEscolhido:
+        print ("Boa meu")
+    else :
+        print ("O numero eh menor")
+
